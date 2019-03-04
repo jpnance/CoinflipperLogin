e41106e12285603156156cf2f71902065ae80554
Patrick Nance <jpnance@gmail.com>
2019-03-03 21:10:50 -0800

    Initial setup that proves dotenv is working.

---
+++ index.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+console.log(port);
+process.exit();
