bb7c46291b6368493a3e3f4314bda363b86dc3d0
Patrick Nance <jpnance@gmail.com>
2019-03-04 21:00:52 -0800

    Locking down the user creation service to requests that provide a good admin key.

--- services/users.js
+++ services/users.js
@@ -1,7 +1,10 @@
 const User = require('../models/user');
 
 module.exports.create = (request, response) => {
-	if (!request.body.email) {
+	if (!request.body.adminKey || request.body.adminKey != process.env.ADMIN_KEY) {
+		response.status(401).send({ error: 'No authorization key provided.' });
+	}
+	else if (!request.body.email) {
 		response.status(400).send({ error: 'No email address provided.' });
 	}
 	else if (!User.validateEmail(request.body.email)) {
