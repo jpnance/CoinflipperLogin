2bdb0974d4994d5f058a4e6fa148ecc8479e8eab
Patrick Nance <jpnance@gmail.com>
2020-09-07 16:40:01 -0700

    We need to be a little smarter about how we do allowed domains.

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const allowedDomains = process.env.ALLOWED_DOMAINS.split(/,/);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const allowCoinflipperSites = (request, response, next) => {
-	response.set('Access-Control-Allow-Origin', process.env.ALLOWED_DOMAINS);
+	if (allowedDomains.includes(request.headers.origin)) {
+		response.set('Access-Control-Allow-Origin', request.headers.origin);
+	}
+
 	next();
 };
 
