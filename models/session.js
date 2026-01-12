59c921428728ca44f83e0661570c926ddfc468c0
Patrick Nance <jpnance@gmail.com>
2026-01-12 15:41:37 -0800

    Adding a TTL index to sessions on lastActivity. Cool feature.

--- models/session.js
+++ models/session.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const sessionSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 	key: { type: String, required: true, unique: true },
-	lastActivity: { type: Date, default: Date.now },
+	lastActivity: { type: Date, default: Date.now, expires: '365d' },
 	pretendingToBe: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
